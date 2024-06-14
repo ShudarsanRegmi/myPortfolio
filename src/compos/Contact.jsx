937983d3bd582ec2ba84e8f301f1ce82a3f929e0
Shudarsan Regmi <shudarsanregmi555@gmail.com>
2024-06-14 15:59:50 +0530

    Contact form was redesigned

--- src/compos/Contact.jsx
+++ src/compos/Contact.jsx
@@ -1,5 +1,4 @@
-import { TextField } from "@mui/material"
-import { TextareaAutosize } from "@mui/material"
+
 import mailIcon from "/mail.png"
 import styles from './Contact.module.css'
 import { Textarea } from "@nextui-org/react";
@@ -7,7 +6,8 @@ import { Input } from "@nextui-org/input";
 import { Button } from "@nextui-org/react";
 import { IoSend } from "react-icons/io5";
 import { useState } from "react";
-
+import { IoIosMailOpen } from "react-icons/io";
+import { IoMailOutline } from "react-icons/io5";
 
 
 
@@ -56,17 +56,26 @@ export const Contact = () => {
         {submitted && <p>email sent successfully</p>}
         <div className={styles.con}>
             <div className={styles.leftCon}>
-                <img src={mailIcon} alt="" />
+                {/* <img src={mailIcon} alt="Mail Image" /> */}
+                {/* <IoIosMailOpen size={500} color={"#fff"}/> */}
+                <IoMailOutline size={500} color="#70b59c"/>
+
+
             </div>
             <div className={styles.rightCon}>
+            <h1 className="text-4xl font-bold text-[#70b59c]">Send me an email</h1>
+
                 <form onSubmit={submit}>
                     <Input type="name" label="Name" id="email"  value={name}
                         onChange={(e) => setName(e.target.value)}
                         required
+                        
 
                     />
                     <Input type="email" label="Email" id="name" value={email}
-                        onChange={(e) => setEmail(e.target.value)}
+                        onChange={(e) => setEmail(e.target.value)} required 
+                        errorMessage={"Enter valid email"}
+
 
                     />
 
@@ -82,7 +91,7 @@ export const Contact = () => {
 
 
                     />
-                    <Button endContent={<IoSend />} type="submit">
+                    <Button className="hover:bg-[#70b59c]" endContent={<IoSend />} type="submit">
                         Send
                     </Button>
 
