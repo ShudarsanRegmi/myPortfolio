805f3094c5c977b87ffcdf8536be91755dcd268b
Shudarsan Regmi <shudarsanregmi555@gmail.com>
2024-06-16 15:25:05 +0530

    Footer links were added

--- src/compos/Footer.jsx
+++ src/compos/Footer.jsx
@@ -7,20 +7,17 @@ export const Footer =()=> {
         <div className={styles.con}>
             <h1 className='text-4xl font-bold pb-9 pt-6'>Thanks for your visit</h1>
             <div className={styles.linksCon}>
-                <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+                <a href="https://twitter.com/ShudarsanRegmi" target="_blank" rel="noopener noreferrer">
                     <FaTwitter size={30}/>
                 </a>
-                <a href="https://github.com" target="_blank" rel="noopener noreferrer">
+                <a href="https://github.com/ShudarsanRegmi" target="_blank" rel="noopener noreferrer">
                     <FaGithub size={30}/>
                 </a>
-                <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+                <a href="https://www.instagram.com/shudarsanregmi/" target="_blank" rel="noopener noreferrer">
                     <FaInstagram size={30}/>
                 </a>
-                <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer">
+                <a href="https://www.linkedin.com/in/ShudarsanRegmi/" target="_blank" rel="noopener noreferrer">
                     <FaLinkedin size={30}/>
-                </a>
-                <a href="https://discord.com" target="_blank" rel="noopener noreferrer">
-                    <FaDiscord size={30}/>
                 </a>
             </div>
         </div>
