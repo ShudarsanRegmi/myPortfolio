1e74ac71688dfe00a9f3d40ca674c9b3849b9cd3
Shudarsan Regmi <shudarsanregmi555@gmail.com>
2024-06-22 07:09:05 +0530

    Favicon was updated

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,6 @@ import { About } from './compos/About'
 import { Techstack } from './compos/Techstack'
 import {Projects} from './compos/Projects'
 import AnimatedList from './compos/AnimatedList'
-import htmlIcon from '/html.svg'
-import vite from '/vite.svg'
 import VerticalAccordion from './compos/VerticalAccordion'
 import {Social} from './compos/Social'
 import {Contact} from './compos/Contact'
@@ -18,8 +16,6 @@ function App() {
       <Navbar />
       <Herosection />
       <About />
-      {/* <AnimatedList /> */}
-      {/* <Projects /> */}
       <VerticalAccordion />
       <Social />
       <Contact />
